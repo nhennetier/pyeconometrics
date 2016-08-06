60b652e6099012a3e751b71855f2f3d1801288f4
nicolashennetier <nicolashennetier2@gmail.com>
2016-08-06 17:37:02 +0200

    Multiple fix about input data

--- panel_models.py
+++ panel_models.py
@@ -51,44 +51,52 @@ def nCr(n,r):
         return 101
     
 
+class BaseModel():
+    def input_data_preparation(self, X):
+        try:
+            X = X.to_frame()
+        except:
+            if len(X.index.names) != 2:
+                raise ValueError("Only 2-level MultiIndex and Panel are supported.")
+
+        X.insert(0, 'constant', 1)
+
+        return X
 
     
-class FEPanelLogit():
+class FixedEffectPanelLogit(BaseModel):
     def response_function(self, X, params):
-        try:
-            X.drop(self.group_by_key, axis=1, inplace=True)
-        except:
-            pass
-        try:
-            X.drop('output', axis=1, inplace=True)
-        except:
-            pass
+        X.drop(self.output, axis=1, inplace=True)
+        
         Z = 0
         for i,var in enumerate(self.variables):
             Z += params[i] * X[var]
+
         return Z.rename('response')
         
     def log_likelihood_student(self, X, y, params):
-        if len(X)!=len(y):
-            print('Training dataset and output should be of same size.')
-            return None
         X.reset_index(drop=True,inplace=True)
         y.reset_index(drop=True,inplace=True)
-        Z = np.array(self.response_function(X,params))
+
+        Z = np.array(self.response_function(X, params))
+
         if nCr(len(y),sum(y)) <= 100:
             perms = unique_permutations(y)
         else:
             perms = [np.random.permutation(y) for _ in range(100)]
+
         result = []
         for a in perms:
             result.append(np.exp(Z.dot(a)))
+
         result = Z.dot(np.array(y)) - log(sum(result))
         return result
             
-    def log_likelihood(self, X, y, params):
-        X['output'] = y
-        result = sum(np.array(X.groupby(self.group_by_key).apply(lambda group : \
-            self.log_likelihood_student(group,group['output'],params))))
+    def log_likelihood(self, X, params):
+        result = sum(np.array(X.apply(lambda group : \
+            self.log_likelihood_student(group,
+            group[self.output], params))))
+
         return result
         
     def conditional_probability(self, X, y, params):
@@ -96,62 +104,57 @@ class FEPanelLogit():
             perms = unique_permutations(y)
         else:
             perms = [np.random.permutation(y) for _ in range(100)]
+
         result = []
         for z in perms:
             result.append(exp(np.array(z).T.dot(np.array(X).dot(params))))
+
         result = np.sum(np.array(result), axis=0)
         result = exp(np.array(y).T.dot(np.array(X).dot(params))) / result
+
         return result
     
     def score_student(self, X, y, params):
-        if len(X)!=len(y):
-            print('Training dataset and output should be of same size.')
-            return None
-        try:
-            X.drop(self.group_by_key, axis=1, inplace=True)
-        except:
-            pass
-        try:
-            X.drop('output', axis=1, inplace=True)
-        except:
-            pass
+        X.drop(self.output, axis=1, inplace=True)
+
         X.reset_index(drop=True,inplace=True)
         y.reset_index(drop=True,inplace=True)
-        if sum(y)==0 or sum(y)==len(y):
+
+        if sum(y) == 0 or sum(y) == len(y):
             return np.array([0 for _ in range(len(X.columns))])
+
         else:
             if nCr(len(y),sum(y)) <= 100:
                 perms = unique_permutations(y)
             else:
                 perms = [np.random.permutation(y) for _ in range(100)]
+
             result = []
             for z in perms:
-                result.append(np.array(z) * self.conditional_probability(X,z,params))
+                result.append(np.array(z) \
+                    * self.conditional_probability(X,z,params))
+
             result = np.sum(np.array(result), axis=0)
             result = np.array(X).T.dot(np.array(y) - result)
+
             return result
             
     def hessian_student(self, X, y, params):
-        if len(X)!=len(y):
-            print('Training dataset and output should be of same size.')
-            return None
-        try:
-            X.drop(self.group_by_key, axis=1, inplace=True)
-        except:
-            pass
-        try:
-            X.drop('output', axis=1, inplace=True)
-        except:
-            pass
+        X.drop(self.output, axis=1, inplace=True)
+
         X.reset_index(drop=True,inplace=True)
         y.reset_index(drop=True,inplace=True)
-        if sum(y)==0 or sum(y)==len(y):
-            return np.array([[0 for _ in range(len(X.columns))] for _ in range(len(X.columns))])
+
+        if sum(y) == 0 or sum(y) == len(y):
+            return np.array([[0 for _ in range(len(X.columns))] \
+                for _ in range(len(X.columns))])
+
         else:
             if nCr(len(y),sum(y)) <= 100:
                 perms = unique_permutations(y)
             else:
                 perms = [list(np.random.permutation(y)) for _ in range(100)]
+
             probas = []
             esp = []
             result = []
@@ -161,134 +164,120 @@ class FEPanelLogit():
                 esp.append(np.array(z) * probas[i])
                 result.append(np.array(z).dot(np.array(z).T) * probas[i])
                 i += 1
+
             esp = np.sum(np.array(esp), axis=0)
             result = np.sum(np.array(result), axis=0)
             result = np.array(X).T.dot(
                 result - esp.T.dot(esp)).dot(np.array(X))
+
             return result
 
-    def fit(self, X, y, group_by_key, nb_iter=50):
-        X['output'] = y
-        X['constant'] = 1
+    def fit(self, X, output, nb_iter=50):
+        self.output = output
+        X = self.input_data_preparation(X)
         
         self.nb_obs = len(X)
-        self.variables = [x for x in X.columns if (x!=group_by_key and x!='output')]
-        self.group_by_key = group_by_key
+        self.variables = [x for x in X.columns if x!=self.output]
         
         params_init = [0 for _ in range(len(self.variables))]   
         self.params_est = np.zeros((nb_iter,len(params_init)))
         self.params_est[0] = params_init
-        self.init_ll = self.log_likelihood(X,y,params_init)
+
+        X = X.groupby(level=0)
+
+        self.init_ll = self.log_likelihood(X, params_init)
         print('Initial log-likelihood : '+ str(self.init_ll))
         print('Parameters estimation in progress.')
         
         j = 1
-        while (j < nb_iter) and (j == 1 or sqrt(sum([(self.params_est[j,k] \
-            - self.params_est[j-1,k])**2 for k in range(len(params_init))])) > 0.05):
+        while (j < nb_iter) and (j == 1 \
+                or self.log_likelihood(X, self.params_est[j-1]) \
+                - self.log_likelihood(X, self.params_est[j-2]) \
+                > 0.01):
             
-            score = sum(np.array(X.groupby(group_by_key).apply(lambda group : \
-                self.score_student(group, group['output'],
+
+            score = sum(np.array(X.apply(lambda group : \
+                self.score_student(group, group[self.output],
                 self.params_est[j-1]))))
 
-            hessian = sum(np.array(X.groupby(group_by_key).apply(lambda group : \
-                self.hessian_student(group,group['output'],self.params_est[j-1]))))
+            hessian = sum(np.array(X.apply(lambda group : \
+                self.hessian_student(group,group[self.output],
+                self.params_est[j-1]))))
 
             try:
-                self.params_est[j] = self.params_est[j-1] + inv(hessian).dot(score)                
-                print('Iteration '+str(j)+', log_likelihood : '\
-                    + str(self.log_likelihood(X,y,self.params_est[j])))
+                self.params_est[j] = self.params_est[j-1] \
+                    + inv(hessian).dot(score)                
+                print('Iteration %s, log_likelihood : %s'\
+                    % (j, self.log_likelihood(X, self.params_est[j])))
                 j += 1
+
             except:
-                print('Improper classification problem')
-                return self
-        self.params = self.params_est[-1]
-        self.final_ll = self.log_likelihood(X,y,self.params)
+                raise ValueError('Improper classification problem' \
+                    + ', should be 2 different labels')
+
+        self.params = self.params_est[j-2]
+        self.final_ll = self.log_likelihood(X, self.params)
+
         if j < nb_iter:
             self.converged = True
         else:
             self.converged = False
+
         return self
         
     def predict(self, X):
         try :
             self.params
         except:
-            print('Fit method should be called before evaluation of the model.')
-            print('The following model is a benchmark with all coefficients normalized.')
-            self.params = [1 for _ in range(4)]
-        Z = self.response_function(X,self.params)
+            raise AttributeError('Fit method should be called before evaluating of the model')
+
+        Z = self.response_function(X, self.params)
         result = (np.sign(Z)+1)/2
+
         return result.astype(int).rename('predicted_values')
         
     def predict_proba(self, X):
         try :
             self.params
         except:
-            print('Fit method should be called before evaluation of the model.')
-            print('The following model is a benchmark with all coefficients normalized.')
-            self.params = [1 for _ in range(4)]
+            raise AttributeError('Fit method should be called before evaluating of the model')
+
         Z = self.response_function(X,self.params)
         return Z.apply(lambda x : norm_cdf(x))
         
-    def beta(self):
+    def __beta(self):
         try:
             return self.params
         except:
-            print('Fit method should be called before evaluation of the model.')
+            raise AttributeError('Fit method should be called before evaluating the model')
             
-    def beta_std(self):
+    def __beta_std(self):
         try:
-            return None
+            return self.params
         except:
-            print('Fit method should be called before evaluation of the model.')
+            raise AttributeError('Fit method should be called before evaluating the model')
             
-    def plot_est(self):
+    def plot_trace_estimators(self):
         try:
             self.params
         except:
-            print('Fit method should be called before evaluation of the model.')
-            return
+            raise AttributeError('Fit method should be called before evaluating of the model')
             
         colors = ['b','g','r','c','m','y','k']
         for k in range(len(self.params)):
-            plt.plot(np.arange(1,len(self.params_est)+1),self.params_est[:,k],
-                color=colors[k-1],label="Beta_"+str(k))
+            plt.plot(np.arange(1, len(self.params_est)+1),
+                     self.params_est[:,k],
+                     color=colors[(k-1) % len(colors)],
+                     label="Beta_%s" % k)
+
         plt.xlim((1,len(self.params_est)*1.2))
         plt.xlabel('Iterations')
         plt.ylabel('Estimators')
         plt.title('Trace plot of estimators of beta', size=16)
         plt.legend(loc='best')
-        
-    def summary(self):
-        try:
-            self.params
-        except:
-            print('Fit method should be called before evaluation of the model.')
-            return
-            
-        print('Logit Regression on Panel Data')
-        print('============================================================')
-        print('%-30s %-30s' \
-            % ('Dep. Variable: has_applied','No. Observations: %s' % str(self.nb_obs)))
-        print('%-30s %-30s' \
-            % ('Model: Panel Logit','Df Model: %s' % str(len(self.params))))
-        print('%-30s %-30s' \
-            % ('Method: MLE','Pseudo R-squ.: %s' 
-            % ("%.5f" % (1-self.final_ll/self.init_ll))))
-        print('%-30s %-30s' \
-            % ('Converged: %s' % self.converged,'Log-Likelihood: %s' 
-            % ("%.1f" % self.final_ll)))
-        print('%-30s %-30s' \
-            % (' ','LL-Null: %s' % ("%.1f" % self.init_ll)))
-        print('%-30s %-30s' \
-            % (' ','LLR p-value: %s' \
-            % ("%.3f" % (1-st.chi2.cdf(-2*log(1-self.final_ll/self.init_ll),
-            len(self.params))))))
-        print('============================================================\n')
-#        length = max([len(x) for x in self.variables if (x != self.group_by_key and x != 'output')])
-#        print('%-*s %-8s %-8s %-8s %-8s %-18s' \
-#              % ((length+4,' '), 'coef', 'std err', 't', 'P>|t|', '[95.0% Conf. Int.]'))
-#        for i,var in enumerate([x for x in self.variables if (x != self.group_by_key and x != 'output')]):
-#            print('%-*s %-8s %-8s %-8s %-8s %-8s %-8s' \
-#                % (length+4,var, self.params[i], 0, 0, 0, 0, 0))
+        plt.show()
+
+    
+
+
         
