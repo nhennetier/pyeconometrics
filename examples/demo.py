d7e3c9d71fd21cfb99dfc78047765ddb55c30978
nicolashennetier <nicolashennetier2@gmail.com>
2016-08-07 21:08:40 +0200

    Add example

---
+++ examples/demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+import pandas as pd
+
+from panel_econometrics.models import FixedEffectPanelLogit
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+
+filepath = os.path.dirname(os.path.abspath(__file__))
+    
+df = pd.read_csv(filepath + '/dataset/jtrain.csv')
+df.set_index(['fcode','year'], inplace=True)
+
+train, test = train_test_split(df, test_size = 0.2, random_state=1995)
+
+FE = FixedEffectPanelLogit()
+FE.fit(train, 'hrsemp', verbose=True)
+
+FE.summary()
+
+test.dropna(inplace=True)
+y_pred = FE.predict(test[['grant', 'employ', 'sales']])
+print('Accuracy score: %s' % accuracy_score(test['hrsemp'], y_pred))
