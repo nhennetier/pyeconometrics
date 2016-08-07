f0fbe64181a654c5bf79ac377b1f334bf55c3b5c
nicolashennetier <nicolashennetier2@gmail.com>
2016-08-07 18:19:13 +0200

    Fix package name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='mord',
+    name='panel_econometrics',
     version='0.1',
     description='Econometrics for Panel Data',
     long_description=open('README.md').read(),
     author='Nicolas HENNETIER',
     author_email='nicolashennetier2@gmail.com',
-    packages=['mord'],
+    packages=['panel_econometrics'],
     requires=['numpy', 'pandas', 'scipy', 'matplotlib']
 )
