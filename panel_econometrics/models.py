df67a9e2df4d7afcc1f23c91c4e6d0ccb051649f
nicolashennetier <nicolashennetier2@gmail.com>
2016-08-07 21:09:20 +0200

    Handle missing values + add summary + basic refactor

--- panel_econometrics/models.py
+++ panel_econometrics/models.py
@@ -3,26 +3,209 @@ import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
+import warnings
+warnings.filterwarnings('ignore')
+
 from numpy.linalg import inv
 from math import exp, sqrt, log
+
+from panel_econometrics.utils import nCr, norm_cdf, unique_permutations
 
     
   
 
 class BaseModel():
-    def input_data_preparation(self, X):
+    def input_data_preparation(self, X, drop_na=None, fill_value=None):
         try:
             X = X.to_frame()
         except:
             if len(X.index.names) != 2:
                 raise ValueError("Only 2-level MultiIndex and Panel are supported.")
 
-        X.insert(0, 'constant', 1)
+        if drop_na is not None:
+            if drop_na:
+                X.dropna(inplace=True)
+            elif fill_value == 'mean':
+                X.fillna(X.mean(), inplace=True)
+            elif fill_value == 'median':
+                X.fillna(X.median(), inplace=True)
+            elif fill_value is not None:
+                for var in X.columns:
+                    try:
+                        X[var].fillna(fill_value.get(var), inplace=True)
+                    except:
+                        raise ValueError('\'fill_value\' argument must be in list ' \
+                            + '[\'mean\', \'median\'] or of type dict. See docstring for more info.')
+
+        X.insert(0, 'const', 1)
 
         return X
 
+    def fit(self, X, output, nb_iter=20, drop_na=True, fill_value=None, verbose=False):
+        self.output = output
+        X = self.input_data_preparation(X, drop_na, fill_value)
+
+        labels = list(np.unique(X[self.output]))
+        if labels != [0,1]:
+            raise ValueError("Labels must be in the unit interval.")
+        
+        self.nb_obs = len(X)
+        self.variables = [x for x in X.columns if x!=self.output]
+        
+        beta_init = [0 for _ in range(len(self.variables))]   
+        self.beta_est = np.zeros((nb_iter,len(beta_init)))
+        self.beta_est[0] = beta_init
+
+        X = X.groupby(level=0)
+
+        self.init_ll = self.log_likelihood(X, beta_init)
+
+        if verbose:
+            print('Initial log-likelihood : '+ str(self.init_ll))
+            print('Parameters estimation in progress.')
+        
+        j = 1
+        while (j < nb_iter) and (j == 1 \
+                or self.log_likelihood(X, self.beta_est[j-1]) \
+                - self.log_likelihood(X, self.beta_est[j-2]) \
+                > 0.01):
+            
+            score = self.score(X, self.beta_est[j-1])
+
+            hessian = self.hessian(X, self.beta_est[j-1])
+
+            try:
+                self.beta_est[j] = self.beta_est[j-1] \
+                    - inv(hessian).dot(score)
+                if verbose:              
+                    print('Iteration %s, log_likelihood : %s'\
+                        % (j, self.log_likelihood(X, self.beta_est[j])))
+                j += 1
+
+            except:
+                raise ValueError('Improper classification problem' \
+                    + ', should be 2 different labels')
+
+        self.beta = self.beta_est[j-2]
+        self.beta_est = self.beta_est[:j-1,:]
+
+        sqrt_vec = np.vectorize(sqrt)
+        hessian = self.hessian(X, self.beta_est[j-2])
+        self.beta_se = sqrt_vec(-inv(hessian).diagonal())
+
+        self.confidence_interval = np.array(
+                [[self.beta[i] - st.norm.ppf(0.975) * self.beta_se[i],
+                    self.beta[i] + st.norm.ppf(0.975) * self.beta_se[i]]
+                    for i in range(len(self.beta))])
+
+        self.final_ll = self.log_likelihood(X, self.beta)
+
+        if j < nb_iter:
+            self.converged = True
+        else:
+            self.converged = False
+
+        return self
+        
+    def plot_trace_estimators(self):
+        try:
+            self.beta
+        except:
+            raise AttributeError('Fit method should be called before evaluating of the model')
+            
+        colors = ['b','g','r','c','m','y','k']
+        for k in range(len(self.beta)):
+            plt.plot(np.arange(1, len(self.beta_est)+1),
+                     self.beta_est[:,k],
+                     color=colors[(k-1) % len(colors)],
+                     label="Beta_%s" % k)
+
+        plt.xlim((1,len(self.beta_est)*1.2))
+        plt.xlabel('Iterations')
+        plt.ylabel('Estimators')
+        plt.title('Trace plot of estimators of beta', size=16)
+        plt.legend(loc='best')
+        plt.show()
+
+    def predict(self, X):
+        try :
+            self.beta
+        except:
+            raise AttributeError('Fit method should be called before evaluating the model.')
+
+        X = self.input_data_preparation(X)
+
+        Z = self.response_function(X, self.beta)
+        result = (np.sign(Z)+1)/2
+
+        try:
+            result = result.astype(int).rename('predicted_label')
+        except:
+            raise ValueError('One or several data are missing.')
+
+        return result
+        
+    def predict_proba(self, X):
+        try :
+            self.beta
+        except:
+            raise AttributeError('Fit method should be called before evaluating the model.')
+
+        X = self.input_data_preparation(X)
+
+        Z = self.response_function(X,self.beta)
+        return Z.apply(lambda x : norm_cdf(x))
+
+    def summary(self):
+        try:
+            self.beta
+        except:
+            raise AttributeError('Fit method should be called before evaluating of the model.')
+            
+        print('='*80)
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Dep. Variable:', self.output, ' ', 'Pseudo R-squ.:', 
+                "%.5f" % (1-self.final_ll/self.init_ll)))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Model:', self.name, ' ', 'Log-Likelihood:', "%.3f" % self.final_ll))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Method:', 'MLE', ' ', 'LL-Null:', "%.3f" % self.init_ll))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('No. Observations:', self.nb_obs, ' ', 'LLR p-value:',
+                "%.3f" % (1-st.chi2.cdf(2 * (self.final_ll - self.init_ll),
+                len(self.beta)))))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Df Model:', len(self.beta)-1, ' ', ' ', ' '))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Converged:', self.converged, ' ', ' ', ' '))
+        print('='*80)
+
+        length = max([len(x) for x in self.variables if x != self.output])
+        print('%25s %8s %8s %8s %8s %18s' \
+            % (' ', 'coef', 'std err', 't', 'P>|t|', '[95.0% Conf. Int.]'))
+        print('-'*80)
+
+        for i,var in enumerate([x for x in self.variables if x != self.output]):
+            print('%-24s %8s %8s %8s %8s %9s %9s' \
+                % (var, 
+                   "%.4f" % self.beta[i], 
+                   "%.3f" % self.beta_se[i], 
+                   "%.3f" % (self.beta[i] / self.beta_se[i]), 
+                   "%.3f" % (1-st.norm.cdf(abs(self.beta[i]) / self.beta_se[i])), 
+                   "%.3f" % self.confidence_interval[i, 0], 
+                   "%.3f" % self.confidence_interval[i, 1])
+                )
+        print('='*80)
+
+
+
+
+
     
 class FixedEffectPanelLogit(BaseModel):
+    def __init__(self):
+        self.name = 'Panel Fixed Effects Logit'
+
     def response_function(self, X, beta):
         try:
             X.drop(self.output, axis=1, inplace=True)
@@ -35,7 +218,7 @@ class FixedEffectPanelLogit(BaseModel):
 
         return Z.rename('response')
         
-    def log_likelihood_student(self, X, y, beta):
+    def log_likelihood_obs(self, X, y, beta):
         X.reset_index(drop=True,inplace=True)
         y.reset_index(drop=True,inplace=True)
 
@@ -55,7 +238,7 @@ class FixedEffectPanelLogit(BaseModel):
             
     def log_likelihood(self, X, beta):
         result = sum(np.array(X.apply(lambda group : \
-            self.log_likelihood_student(group,
+            self.log_likelihood_obs(group,
             group[self.output], beta))))
 
         return result
@@ -75,7 +258,7 @@ class FixedEffectPanelLogit(BaseModel):
 
         return result
     
-    def score_student(self, X, y, beta):
+    def score_obs(self, X, y, beta):
         X.drop(self.output, axis=1, inplace=True)
 
         X.reset_index(drop=True,inplace=True)
@@ -102,10 +285,10 @@ class FixedEffectPanelLogit(BaseModel):
 
     def score(self, X, beta):
         return sum(np.array(X.apply(lambda group : \
-            self.score_student(group, group[self.output],
+            self.score_obs(group, group[self.output],
             beta))))
             
-    def hessian_student(self, X, y, beta):
+    def hessian_obs(self, X, y, beta):
         X.drop(self.output, axis=1, inplace=True)
 
         X.reset_index(drop=True,inplace=True)
@@ -140,114 +323,7 @@ class FixedEffectPanelLogit(BaseModel):
 
     def hessian(self, X, beta):
         return sum(np.array(X.apply(lambda group : \
-            self.hessian_student(group,group[self.output],
+            self.hessian_obs(group,group[self.output],
             beta))))
-
-    def fit(self, X, output, nb_iter=50):
-        self.output = output
-        X = self.input_data_preparation(X)
-
-        labels = list(np.unique(X[self.output]))
-        if labels != [0,1]:
-            raise ValueError("Labels must be in the unit interval.")
-        
-        self.nb_obs = len(X)
-        self.variables = [x for x in X.columns if x!=self.output]
-        
-        beta_init = [0 for _ in range(len(self.variables))]   
-        self.beta_est = np.zeros((nb_iter,len(beta_init)))
-        self.beta_est[0] = beta_init
-
-        X = X.groupby(level=0)
-
-        self.init_ll = self.log_likelihood(X, beta_init)
-        print('Initial log-likelihood : '+ str(self.init_ll))
-        print('Parameters estimation in progress.')
-        
-        j = 1
-        while (j < nb_iter) and (j == 1 \
-                or self.log_likelihood(X, self.beta_est[j-1]) \
-                - self.log_likelihood(X, self.beta_est[j-2]) \
-                > 0.01):
-            
-            score = self.score(X, self.beta_est[j-1])
-
-            hessian = self.hessian(X, self.beta_est[j-1])
-
-            try:
-                self.beta_est[j] = self.beta_est[j-1] \
-                    - inv(hessian).dot(score)                
-                print('Iteration %s, log_likelihood : %s'\
-                    % (j, self.log_likelihood(X, self.beta_est[j])))
-                j += 1
-
-            except:
-                raise ValueError('Improper classification problem' \
-                    + ', should be 2 different labels')
-
-        self.beta = self.beta_est[j-2]
-        self.beta_est = self.beta_est[:j-1,:]
-
-        sqrt_vec = np.vectorize(sqrt)
-        hessian = self.hessian(X, self.beta_est[j-2])
-        self.beta_se = sqrt_vec(-inv(hessian).diagonal())
-
-        self.confidence_interval = np.array(
-                [[self.beta[i] - st.norm.ppf(0.975) * self.beta_se[i],
-                    self.beta[i] + st.norm.ppf(0.975) * self.beta_se[i]]
-                    for i in range(len(self.beta))])
-
-        self.final_ll = self.log_likelihood(X, self.beta)
-
-        if j < nb_iter:
-            self.converged = True
-        else:
-            self.converged = False
-
-        return self
-        
-    def predict(self, X):
-        try :
-            self.beta
-        except:
-            raise AttributeError('Fit method should be called before evaluating of the model')
-
-        X = self.input_data_preparation(X)
-
-        Z = self.response_function(X, self.beta)
-        result = (np.sign(Z)+1)/2
-
-        return result.astype(int).rename('predicted_label')
-        
-    def predict_proba(self, X):
-        try :
-            self.beta
-        except:
-            raise AttributeError('Fit method should be called before evaluating of the model')
-
-        X = self.input_data_preparation(X)
-
-        Z = self.response_function(X,self.beta)
-        return Z.apply(lambda x : norm_cdf(x))
-        
-    def plot_trace_estimators(self):
-        try:
-            self.beta
-        except:
-            raise AttributeError('Fit method should be called before evaluating of the model')
-            
-        colors = ['b','g','r','c','m','y','k']
-        for k in range(len(self.beta)):
-            plt.plot(np.arange(1, len(self.beta_est)+1),
-                     self.beta_est[:,k],
-                     color=colors[(k-1) % len(colors)],
-                     label="Beta_%s" % k)
-
-        plt.xlim((1,len(self.beta_est)*1.2))
-        plt.xlabel('Iterations')
-        plt.ylabel('Estimators')
-        plt.title('Trace plot of estimators of beta', size=16)
-        plt.legend(loc='best')
-        plt.show()
 
         
