b2225cc5931c7b7c64422123c0855a3da6181296
nicolashennetier <nicolas.hennetier2@gmail.com>
2016-08-25 17:49:48 +0200

    Test

--- examples/demo_panel_discrete.py
+++ examples/demo_panel_discrete.py
@@ -11,32 +11,28 @@ filepath = os.path.dirname(os.path.abspath(__file__))
 df = pd.read_csv(filepath + '/dataset/jtrain.csv')
 df.set_index(['fcode','year'], inplace=True)
 
-# df = df.head(12)
-# train = df.head(9)
-# test = df.ix[9:]
-
 train, test = train_test_split(df, test_size = 0.2, random_state=1995)
 test.dropna(inplace=True)
 
 
 #1. Fixed Effects Logit Model
-# FE = FixedEffectPanelModel()
-# FE.fit(train, 'hrsemp', verbose=True)
-# FE.summary()
+FE = FixedEffectPanelModel()
+FE.fit(train, 'hrsemp', verbose=True)
+FE.summary()
 
-# y_pred = FE.predict(test[['grant', 'employ', 'sales']])
-# print('FE Logit, accuracy score: %s' % accuracy_score(test['hrsemp'], y_pred))
+y_pred = FE.predict(test[['grant', 'employ', 'sales']])
+print('FE Logit, accuracy score: %s' % accuracy_score(test['hrsemp'], y_pred))
 
 #2. Random Effects Probit Model
-# RE = RandomEffectsPanelModel('probit')
-# RE.fit(train, 'hrsemp', verbose=True)
-# RE.summary()
+RE = RandomEffectsPanelModel('probit')
+RE.fit(train, 'hrsemp', verbose=True)
+RE.summary()
 
-# y_pred = RE.predict(test[['grant', 'employ', 'sales']])
-# print('RE Probit, accuracy score: %s' % accuracy_score(test['hrsemp'], y_pred))
+y_pred = RE.predict(test[['grant', 'employ', 'sales']])
+print('RE Probit, accuracy score: %s' % accuracy_score(test['hrsemp'], y_pred))
 
 
-# #3. Random Effects Logit Model
+#3. Random Effects Logit Model
 RE = RandomEffectsPanelModel('logit')
 RE.fit(train, 'hrsemp', verbose=True)
 RE.summary()
