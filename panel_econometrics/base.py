73d28ae9599dfbdb5a33ff482ae46cbabb2bab84
nicolashennetier <nicolas.hennetier2@gmail.com>
2016-08-26 14:27:36 +0200

    Addition of class for results

--- panel_econometrics/base.py
+++ panel_econometrics/base.py
@@ -10,7 +10,66 @@ from panel_econometrics.utils import norm_cdf
 
 
 
-class BaseModel():
+class Results():
+    def model_description(self):
+        print('='*80)
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Dep. Variable:', self.output, ' ', 'Pseudo R-squ.:', 
+                "%.5f" % (1-self.final_ll/self.init_ll)))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Model:', self.name, ' ', 'Log-Likelihood:', "%.3f" % self.final_ll))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Method:', 'MLE', ' ', 'LL-Null:', "%.3f" % self.init_ll))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('No. Observations:', self.nb_obs, ' ', 'LLR p-value:',
+                "%.3f" % (1-st.chi2.cdf(2 * (self.final_ll - self.init_ll),
+                len(self.beta)))))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Df Model:', len(self.beta)-1, ' ', ' ', ' '))
+        print('%-17s %30s %1s %-20s %8s' \
+            % ('Converged:', self.converged, ' ', ' ', ' '))
+        print('='*80)
+
+    def columns_header(self):
+        print('%25s %8s %8s %8s %8s %18s' \
+            % (' ', 'coef', 'std err', 't', 'P>|t|', '[95.0% Conf. Int.]'))
+        print('-'*80)
+
+    def beta_description(self):
+        for i,var in enumerate([x for x in self.variables if x != self.output]):
+            print('%-24s %8s %8s %8s %8s %9s %9s' \
+                % (var, 
+                   "%.4f" % self.beta[i], 
+                   "%.3f" % self.beta_se[i], 
+                   "%.3f" % (self.beta[i] / self.beta_se[i]), 
+                   "%.3f" % (1-st.norm.cdf(abs(self.beta[i]) / self.beta_se[i])), 
+                   "%.3f" % self.confidence_interval[i, 0], 
+                   "%.3f" % self.confidence_interval[i, 1])
+                )
+        print('-'*80)
+
+    def sigma_description(self):
+        print('%-24s %8s %8s %8s %8s %9s %9s' \
+            % ('/sigma', 
+               "%.4f" % self.sigma, 
+               "%.3f" % self.beta_se[-1], 
+               "", 
+               "", 
+               "%.3f" % self.confidence_interval[-1, 0], 
+               "%.3f" % self.confidence_interval[-1, 1])
+            )
+        print('-'*80)
+
+    def censored_data_description(self):
+        print('%27s %-52s' \
+            % ('Obs. summary:',
+                '%s censored observations' % self.nb_censored_obs))
+        print('%27s %-52s' \
+            % ('',
+                '%s uncensored observations' % self.nb_uncensored_obs))
+
+
+class BaseModel(Results):
     '''Base class inherited by other models
     Not intended to be used separately
     '''
@@ -61,7 +120,7 @@ class BaseModel():
         X = self.input_data_preparation(X)
         X.insert(0, '_cons', 1)
 
-        Z = self.__response_function(X, self.beta)
+        Z = self.response_function(X, self.beta)
         result = (np.sign(Z)+1)/2
 
         try:
@@ -78,47 +137,16 @@ class BaseModel():
         X = self.input_data_preparation(X)
         X.insert(0, '_cons', 1)
 
-        Z = self.r__esponse_function(X,self.beta)
+        Z = self.response_function(X,self.beta)
         return Z.apply(lambda x : norm_cdf(x))
 
     def summary(self):
         if self.beta is None:
             raise AttributeError('Fit method should be called before evaluating of the model.')
-            
-        print('='*80)
-        print('%-17s %30s %1s %-20s %8s' \
-            % ('Dep. Variable:', self.output, ' ', 'Pseudo R-squ.:', 
-                "%.5f" % (1-self.final_ll/self.init_ll)))
-        print('%-17s %30s %1s %-20s %8s' \
-            % ('Model:', self.name, ' ', 'Log-Likelihood:', "%.3f" % self.final_ll))
-        print('%-17s %30s %1s %-20s %8s' \
-            % ('Method:', 'MLE', ' ', 'LL-Null:', "%.3f" % self.init_ll))
-        print('%-17s %30s %1s %-20s %8s' \
-            % ('No. Observations:', self.nb_obs, ' ', 'LLR p-value:',
-                "%.3f" % (1-st.chi2.cdf(2 * (self.final_ll - self.init_ll),
-                len(self.beta)))))
-        print('%-17s %30s %1s %-20s %8s' \
-            % ('Df Model:', len(self.beta)-1, ' ', ' ', ' '))
-        print('%-17s %30s %1s %-20s %8s' \
-            % ('Converged:', self.converged, ' ', ' ', ' '))
-        print('='*80)
 
-        length = max([len(x) for x in self.variables if x != self.output])
-        print('%25s %8s %8s %8s %8s %18s' \
-            % (' ', 'coef', 'std err', 't', 'P>|t|', '[95.0% Conf. Int.]'))
-        print('-'*80)
-
-        for i,var in enumerate([x for x in self.variables if x != self.output]):
-            print('%-24s %8s %8s %8s %8s %9s %9s' \
-                % (var, 
-                   "%.4f" % self.beta[i], 
-                   "%.3f" % self.beta_se[i], 
-                   "%.3f" % (self.beta[i] / self.beta_se[i]), 
-                   "%.3f" % (1-st.norm.cdf(abs(self.beta[i]) / self.beta_se[i])), 
-                   "%.3f" % self.confidence_interval[i, 0], 
-                   "%.3f" % self.confidence_interval[i, 1])
-                )
-        print('='*80)
+        self.model_description()
+        self.columns_header()
+        self.beta_description()
 
 
 
@@ -134,7 +162,6 @@ class PanelBaseModel(BaseModel):
                 raise ValueError("Only 2-level MultiIndex and Panel are supported.")
 
         X = self.handle_missing_values(X, drop_na, fill_value)
-
         return X
 
 
@@ -142,18 +169,22 @@ class CensoredBaseModel(BaseModel):
     '''Base class inherited by other models
     Not intended to be used separately
     '''
-    def input_data_preparation(self, X, output, drop_na=None, fill_value=None):
-        neg_values = X[X[output] <= 0]
-        if len(neg_values[neg_values[output] < 0]) > 0:
-            raise ValueError("Negative values where found in output variable." \
-                + "Please set all censored observations to 0 before fitting the model.")
-        elif len(neg_values) == 0:
-            raise ValueError("No censored observations were found." \
-                + "Please set output of all censored observations to 0 before fitting the model.")
+    def input_data_preparation(self, X, drop_na=None, fill_value=None):
+        if self.output in X.columns:
+            neg_values = X[X[self.output] <= 0]
+            if len(neg_values[neg_values[self.output] < 0]) > 0:
+                raise ValueError("Negative values where found in output variable." \
+                    + "Please set all censored observations to 0 before fitting the model.")
+            elif len(neg_values) == 0:
+                raise ValueError("No censored observations were found." \
+                    + "Please set output of all censored observations to 0 before fitting the model.")
 
         X = self.handle_missing_values(X, drop_na, fill_value)
-
         return X
 
-        
+    def summary(self):
+        BaseModel.summary(self)
+        self.sigma_description()
+        self.censored_data_description()
+     
         
